ca6a347d6c9d5295723232828181be35eb9db681
Samu27 <samuele.mori.96@gmail.com>
2018-05-20 15:32:57 +0200

    Setup script

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ cp -r gamma ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/.
 echo Select your turtlebot model
 echo export TURTLEBOT3_MODEL=waffle
 
-echo Launch cyber_lab with turtlebot:
-echo roslaunch turtlebot3_gazebo turtlebot3_cyber_lab.launch
+echo Launch gamma with turtlebot:
+echo roslaunch turtlebot3_gazebo turtlebot3_gamma.launch
 
 echo
 echo Then launch Turtlebot autonomous navigation:
